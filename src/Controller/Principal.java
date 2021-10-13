2f91be590d7f855517477c9d1da6cf9d45623119
Leonardo Silva <leonardogabriel8@hotmail.com>
2021-10-13 13:36:15 -0300

    mensagem acrescentada

--- src/Controller/Principal.java
+++ src/Controller/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
     public static void main(String[] args) {
         Consulta consulta = new Consulta();
+        System.out.println("Fiz uma alteração para Github");
 
 
 }
